0ffb9f20ac7d7fc3921d28c1c98622f270f3b621
layen67 <lkbcontact@gmail.com>
2022-12-10 01:34:38 +0100

    Create in.sh

---
+++ in.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+
+apt-get update;apt-get install -y docker.io;
+curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose;
+chmod +x /usr/local/bin/docker-compose;
+
