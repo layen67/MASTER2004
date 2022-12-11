7a1bdbfac6061f35822170244dd78e7910e8f237
layen67 <lkbcontact@gmail.com>
2022-12-11 01:41:48 +0100

    Update in2.sh

--- in2.sh
+++ in2.sh
@@ -169,5 +169,75 @@ services:
 
 cd /opt/postal/config/nginx-proxy;
 docker-compose up -d;
+sleep 30;
+
+
+apt install spamassassin -y;
+apt install git curl jq -y;
+git clone https://postalserver.io/start/install /opt/postal/install;
+ln -s /opt/postal/install/bin/postal /usr/bin/postal;
+
+docker run -d \
+   --name postal-mariadb \
+   -p 127.0.0.1:3306:3306 \
+   --restart always \
+   -e MARIADB_DATABASE=postal \
+   -e MARIADB_ROOT_PASSWORD=$msqlroot \
+   mariadb
+
+docker run -d \
+   --name postal-rabbitmq \
+   -p 127.0.0.1:5672:5672 \
+   --restart always \
+   -e RABBITMQ_DEFAULT_USER=postal \
+   -e RABBITMQ_DEFAULT_PASS=$msqlroot \
+   -e RABBITMQ_DEFAULT_VHOST=postal \
+   rabbitmq:3.8
+   
+postal bootstrap postal.$domainname;
+  
+sed -i -e '/^smtp_server:/d' /opt/postal/config/postal.yml
+sed -i -e '/^  port: 25/d' /opt/postal/config/postal.yml
+
+echo '' | sudo tee -a /opt/postal/config/postal.yml;
+echo 'smtp_server:' | sudo tee -a /opt/postal/config/postal.yml;
+echo '  port: 25' | sudo tee -a /opt/postal/config/postal.yml;
+echo '  tls_enabled: true' | sudo tee -a /opt/postal/config/postal.yml;
+echo '  # tls_certificate_path: ' | sudo tee -a /opt/postal/config/postal.yml;
+echo '  # tls_private_key_path: ' | sudo tee -a /opt/postal/config/postal.yml;
+echo '  proxy_protocol: false' | sudo tee -a /opt/postal/config/postal.yml;
+echo '  log_connect: true' | sudo tee -a /opt/postal/config/postal.yml;
+echo '  strip_received_headers: true' | sudo tee -a /opt/postal/config/postal.yml;
+
+sed -i -e "s/example.com/$domainname/g" /opt/postal/config/postal.yml;
+sed -i -e "s/mx.postal.$domainname/postal.$domainname/g" /opt/postal/config/postal.yml;
+sed -i -e "s/bind_address: 127.0.0.1/bind_address: 0.0.0.0/g" /opt/postal/config/postal.yml;
+sed -i -e "s/password: postal/password: $msqlroot/g" /opt/postal/config/postal.yml;
+
+postal initialize;
+
+postal make-user;
+
+command hostnamectl set-hostname postal.$domainname;
+
+postal stop;
+# docker run --restart=always -d --name phpmyadmin -e PMA_ARBITRARY=1 -p 8080:80 phpmyadmin;
+
+sleep 20
+chmod 777 /opt/postal/config/nginx-proxy/npm/letsencrypt/live -R;
+
+sed -i -r "s/.*tls_certificate_path.*/  tls_certificate_path: \/config\/https\/ssl_certs\/postal.$domainname\/production\/signed.crt/g" /opt/postal/config/postal.yml;
+sed -i -r "s/.*tls_private_key_path.*/  tls_private_key_path: \/config\/https\/ssl_certs\/postal.$domainname\/production\/domain.key/g" /opt/postal/config/postal.yml;
+
+sed -i -e "s/ENABLED=0/ENABLED=1/g" /etc/default/spamassassin;
+systemctl restart spamassassin;
+
+echo '' | sudo tee -a /opt/postal/config/postal.yml;
+echo 'spamd:' | sudo tee -a /opt/postal/config/postal.yml;
+echo '  enabled: true' | sudo tee -a /opt/postal/config/postal.yml;
+echo '  host: 127.0.0.1' | sudo tee -a /opt/postal/config/postal.yml;
+echo '  port: 783' | sudo tee -a /opt/postal/config/postal.yml;
+
+postal start;
 
 
