2cbf825ca5dd253204368aeaa528a3c99e98513e
layen67 <lkbcontact@gmail.com>
2022-12-11 23:20:18 +0100

    Update in3.sh

--- in3.sh
+++ in3.sh
@@ -15,7 +15,7 @@ apt install docker-ce -y;
 
 
 
-curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose;
+curl -L https://github.com/docker/compose/releases/download/2.14.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose;
 chmod +x /usr/local/bin/docker-compose;
 sysctl -w net.ipv4.ip_forward=1;
 
